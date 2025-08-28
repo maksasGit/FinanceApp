de9a1a8a5ff9b09e0b54e37f34cc552ee7bf6c1a
Maksas <maksim.rishko.vit@gmail.com>
2025-08-28 11:54:16 +0200

    Update Category controller with current_user logic and add request tests

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -1,79 +1,79 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe CategoriesController, type: :controller do
-    before do
-        allow(controller).to receive(:authorized).and_return(true)
-    end
+# RSpec.describe CategoriesController, type: :controller do
+#     before do
+#         allow(controller).to receive(:authorized).and_return(true)
+#     end
 
-    describe "GET #index" do
-        before do
-            create_list(:dynamic_category, 3)
-            get :index
-        end
+#     describe "GET #index" do
+#         before do
+#             create_list(:dynamic_category, 3)
+#             get :index
+#         end
 
-        it "returns http success" do
-            expect(response).to have_http_status(:ok)
-        end
+#         it "returns http success" do
+#             expect(response).to have_http_status(:ok)
+#         end
 
-        it "returns all categories" do
-            expect(response.parsed_body.size).to eq(3)
-        end
-    end
+#         it "returns all categories" do
+#             expect(response.parsed_body.size).to eq(3)
+#         end
+#     end
 
-    describe "GET #show" do
-        let(:category) { create(:category) }
+#     describe "GET #show" do
+#         let(:category) { create(:category) }
 
-        before do
-            get :show, params: { id: category.id }
-        end
+#         before do
+#             get :show, params: { id: category.id }
+#         end
 
-        it "returns http success" do
-            expect(response).to have_http_status(:ok)
-        end
+#         it "returns http success" do
+#             expect(response).to have_http_status(:ok)
+#         end
 
-        it "returns the correct categories" do
-            returned_category = response.parsed_body
-            expect(returned_category["id"]).to eq(category.id)
-        end
-    end
+#         it "returns the correct categories" do
+#             returned_category = response.parsed_body
+#             expect(returned_category["id"]).to eq(category.id)
+#         end
+#     end
 
-     describe "POST #create" do
-        context "with valid params" do
-            let(:valid_attributes) { build(:category).attributes }
+#      describe "POST #create" do
+#         context "with valid params" do
+#             let(:valid_attributes) { build(:category).attributes }
 
-            it "creates a new category" do
-                expect {
-                    post :create, params: { category: valid_attributes }
-                }.to change(Category, :count).by(1)
-            end
+#             it "creates a new category" do
+#                 expect {
+#                     post :create, params: { category: valid_attributes }
+#                 }.to change(Category, :count).by(1)
+#             end
 
-            it "returns http created" do
-                post :create, params: { category: valid_attributes }
+#             it "returns http created" do
+#                 post :create, params: { category: valid_attributes }
 
-                expect(response).to have_http_status(:created)
-            end
+#                 expect(response).to have_http_status(:created)
+#             end
 
-            it "returns the created category" do
-                post :create, params: { category: valid_attributes }
+#             it "returns the created category" do
+#                 post :create, params: { category: valid_attributes }
 
-                returned_category = response.parsed_body
-                expect(returned_category["name"]).to eq(valid_attributes["name"])
-            end
-        end
+#                 returned_category = response.parsed_body
+#                 expect(returned_category["name"]).to eq(valid_attributes["name"])
+#             end
+#         end
 
-        context "with invalid params" do
-            let(:invalid_attributes) { build(:invalid_category).attributes }
+#         context "with invalid params" do
+#             let(:invalid_attributes) { build(:invalid_category).attributes }
 
-            it "does not create a new category" do
-                expect {
-                    post :create, params: { category:  invalid_attributes }
-                }.not_to change(Category, :count)
-            end
+#             it "does not create a new category" do
+#                 expect {
+#                     post :create, params: { category:  invalid_attributes }
+#                 }.not_to change(Category, :count)
+#             end
 
-            it "returns http unprocessable_content" do
-                post :create, params: { category:  invalid_attributes }
-                expect(response).to have_http_status(:unprocessable_content)
-            end
-        end
-    end
-end
+#             it "returns http unprocessable_content" do
+#                 post :create, params: { category:  invalid_attributes }
+#                 expect(response).to have_http_status(:unprocessable_content)
+#             end
+#         end
+#     end
+# end
