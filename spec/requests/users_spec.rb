ca543962fca0e105b866c05bb75e2b1af64d4d46
Maksas <maksim.rishko.vit@gmail.com>
2025-08-29 14:16:03 +0200

    Refactoring User Controller and Move test to request type

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,9 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  let(:user) { create(:user) }
-  let(:token) { JWT.encode({ user_id: user.id }, Rails.application.secret_key_base) }
+  let(:current_user) { create(:auth_user) }
+  let(:token) { JWT.encode({ user_id: current_user.id }, Rails.application.secret_key_base) }
   let(:headers) { { 'Authorization' => "Bearer #{token}" } }
+
+  let(:valid_params) do
+    {
+      user: {
+        name: "valid_name",
+        email: "valid@email",
+        password: "validpassword"
+      }
+    }
+  end
+
+  let(:invalid_params) do
+    {
+      user: {
+        name: "",
+        email: nil,
+        password: "1"
+      }
+    }
+  end
+
+  def json_response
+    response.parsed_body
+  end
 
   describe "GET /users" do
     before do
@@ -19,7 +43,69 @@ RSpec.describe "Users", type: :request do
     it "returns all users" do
       get "/users", headers: headers
 
-      expect(response.parsed_body.size).to eq(4) # with auth user
+      expect(json_response.size).to eq(3 + 1) # 3 dynamic_user + 1 current_user
+    end
+  end
+
+  describe "GET /users/:id" do
+    it "returns status ok" do
+      get "/users/#{current_user.id}", headers: headers
+
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "returns 404 if no such user" do
+       get "/users/#{0}", headers: headers
+
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
+  describe "POST /users" do
+    it "returns status created" do
+      post "/users", params: valid_params, headers: headers
+
+      expect(response).to have_http_status(:created)
+    end
+
+    it "returns unprocessable_content" do
+      post "/users", params: invalid_params, headers: headers
+
+      expect(response).to have_http_status(:unprocessable_content)
+    end
+  end
+
+  describe "PUT /users/:id" do
+    it "returns status ok" do
+      put "/users/#{current_user.id}", params: valid_params, headers: headers
+
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "returns status not_found" do
+      put "/users/#{0}", params: valid_params, headers: headers
+
+      expect(response).to have_http_status(:not_found)
+    end
+
+    it "returns status unprocessable_content" do
+      put "/users/#{current_user.id}", params: invalid_params, headers: headers
+
+      expect(response).to have_http_status(:unprocessable_content)
+    end
+  end
+
+  describe "DELETE /users/:id" do
+    it "returns status no_content" do
+      delete "/users/#{current_user.id}", params: valid_params, headers: headers
+
+      expect(response).to have_http_status(:no_content)
+    end
+
+    it "returns status not_found" do
+      delete "/users/#{0}", headers: headers
+
+      expect(response).to have_http_status(:not_found)
     end
   end
 end
