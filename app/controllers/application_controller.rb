ce3e835a52b1336243aeeb1aa2fcbe8f413538d8
maksas <maksim.rishko.vit@gmail.com>
2025-09-03 15:54:53 +0200

    Add token expire time (#35)
    lint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,36 +1,41 @@
 class ApplicationController < ActionController::API
-    before_action :authorized
+  before_action :authorized
 
-    SECRET_KEY = Rails.application.secret_key_base
+  SECRET_KEY = Rails.application.secret_key_base
 
-    def encode_token(payload)
-        JWT.encode(payload, SECRET_KEY)
+  def encode_token(payload)
+    exp = 1.day.from_now.to_i
+    payload = payload.merge({ exp: exp })
+
+    JWT.encode(payload, SECRET_KEY)
+  end
+
+  def decoded_token
+    header = request.headers["Authorization"]
+    if header
+      token = header.split(" ")[1]
+      begin
+        JWT.decode(token, SECRET_KEY, true, algorithm: "HS256")
+      rescue JWT::ExpiredSignature
+        nil
+      rescue JWT::DecodeError
+        nil
+      end
     end
+  end
 
-    def decoded_token
-        header = request.headers["Authorization"]
-        if header
-            token = header.split(" ")[1]
-            begin
-                JWT.decode(token, SECRET_KEY, true, algorithm: "HS256")
-            rescue JWT::DecodeError
-                nil
-            end
-        end
+  def current_user
+    return @current_user if defined?(@current_user)
+
+    if decoded_token
+      user_id = decoded_token[0]["user_id"]
+      @current_user = User.find_by(id: user_id)
     end
+  end
 
-    def current_user
-        return @current_user if defined?(@current_user)
-
-        if decoded_token
-            user_id = decoded_token[0]["user_id"]
-            @current_user = User.find_by(id: user_id)
-        end
+  def authorized
+    unless !!current_user
+      render json: { message: "Please log in" }, status: :unauthorized
     end
-
-    def authorized
-        unless !!current_user
-            render json: { message: "Please log in" }, status: :unauthorized
-        end
-    end
+  end
 end
